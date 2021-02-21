c5d52cdfea17f5dda22e3539b2aba167317a168c
eliasjr <a49888ce3c@emailmonkey.club>
2021-02-21 13:27:33 -0400

    create project

---
+++ src/main/java/br/com/eliasjr/craftbeer/SensediaCraftBeerApplication.java
@@ -0,0 +1,13 @@
+package br.com.eliasjr.craftbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensediaCraftBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensediaCraftBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eliasjr/craftbeer/SensediaCraftBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliasjr.craftbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensediaCraftBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
